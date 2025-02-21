3385f7bbfa2284e0a5bf178dd947f7b61a029f51
Hyunggeun Kim <hgk0000@naver.com>
2025-02-21 15:18:36 +0900

    feat : server project generated

---
+++ server/grouproom/src/main/java/com/manchung/grouproom/GrouproomApplication.java
@@ -0,0 +1,13 @@
+package com.manchung.grouproom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrouproomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrouproomApplication.class, args);
+	}
+
+}
---
+++ server/grouproom/src/test/java/com/manchung/grouproom/GrouproomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manchung.grouproom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrouproomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
