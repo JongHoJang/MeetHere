c92dd814574ef11b46538f088eca5f7d21bf90d0
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2025-05-04 01:26:56 +0900

    style: 중복 flex 삭제, 타이틀이 레이아웃에서 벗어나는 부분 수정

--- client/src/app/(auth)/signup/page.tsx
+++ client/src/app/(auth)/signup/page.tsx
@@ -4,13 +4,13 @@ import SignUpForm from '@/app/(auth)/signup/_component/SignUpForm'
 const SignUpPage = () => {
   return (
     <div className="pt-10 pb-10 md:pb-20">
-      <div className="mx-auto w-full max-w-[1140]">
+      <div className="mx-auto w-full max-w-[1140px]">
         <div className="px-4 md:px-0 pb-4">
           <div className="mb-8 md:mb-12">
             <h2 className="text-2xl md:text-4xl font-bold">회원가입</h2>
           </div>
           <div>
-            <div className="flex items-center justify-center">
+            <div className="items-start">
               <SignUpForm />
             </div>
           </div>
