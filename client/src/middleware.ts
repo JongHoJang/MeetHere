507106a9ff065f7eece6ef5de662667ee84deac5
jangjh9414 <89680082+JongHoJang@users.noreply.github.com>
2025-05-07 19:41:28 +0900

    test: login accessToken httpOnly로 변경 테스트 (middleware 설정 초기화)

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -41,8 +41,9 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function middleware(_request: NextRequest) {
-  return NextResponse.next() // 항상 통과
+  return NextResponse.next()
 }
 
 export const config = {
